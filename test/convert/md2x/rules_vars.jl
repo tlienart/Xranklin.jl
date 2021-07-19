df59d5c5204a788854344e11e2c31a1fdf934e94
Thibaut Lienart <tlienart@me.com>
2021-07-19 22:14:27 +0200

    more tests for vars

--- test/convert/md2x/rules_vars.jl
+++ test/convert/md2x/rules_vars.jl
@@ -1,4 +1,4 @@
-using Xranklin, Test; X = Xranklin
+using Xranklin, Test, Dates; X = Xranklin
 
 @testset "mddef" begin
     X.FRANKLIN_ENV[:CUR_LOCAL_CTX] = nothing
@@ -18,4 +18,24 @@ using Xranklin, Test; X = Xranklin
     X.set_current_local_context(lc)
     @test globvar(:x) == 5
     @test locvar(:x) == 5
+end
+
+@testset "mddef-block" begin
+    lc = X.LocalContext()
+    s = """
+        +++
+        using Dates
+        a = 5
+        b = "hello"
+        c = [1 2
+        3 4]
+        d = Date(1)
+        +++
+        """
+    o = html(s, lc)
+    @test isempty(strip(o))
+    @test value(lc, :a) == 5
+    @test value(lc, :b, "bye") == "hello"
+    @test value(lc, :c) == [1 2; 3 4]
+    @test year(value(lc, :d)) == 1
 end
