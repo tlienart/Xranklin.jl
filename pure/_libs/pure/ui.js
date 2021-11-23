ca88c7aab5d64d37c501c8242d3af38bf78565ec
Thibaut Lienart <tlienart@me.com>
2021-11-23 17:19:58 +0100

    .

--- pure/_libs/pure/ui.js
+++ pure/_libs/pure/ui.js
@@ -1,54 +1,40 @@
 (function (window, document) {
-
-    // we fetch the elements each time because docusaurus removes the previous
-    // element references on page navigation
-    function getElements() {
-        return {
-            layout: document.getElementById('layout'),
-            menu: document.getElementById('menu'),
-            menuLink: document.getElementById('menuLink')
-        };
-    }
-
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/);
-        var length = classes.length;
-        var i = 0;
-
-        for (; i < length; i++) {
-            if (classes[i] === className) {
-                classes.splice(i, 1);
-                break;
-            }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
-    }
-
-    function toggleAll() {
-        var active = 'active';
-        var elements = getElements();
-
-        toggleClass(elements.layout, active);
-        toggleClass(elements.menu, active);
-        toggleClass(elements.menuLink, active);
-    }
-
-    function handleEvent(e) {
-        var elements = getElements();
-
-        if (e.target.id === elements.menuLink.id) {
-            toggleAll();
-            e.preventDefault();
-        } else if (elements.menu.className.indexOf('active') !== -1) {
-            toggleAll();
-        }
-    }
-
-    document.addEventListener('click', handleEvent);
-
-}(this, this.document));
+      function getElements() {
+          return {
+              menu_panel:  document.getElementById('layout-left-menu'),
+              menu_burger: document.getElementById('menu-burger')
+          };
+      }
+      function toggleClass(element, className) {
+          var classes = element.className.split(/\s+/);
+          var length = classes.length;
+          var i = 0;
+          for (; i < length; i++) {
+              if (classes[i] === className) {
+                  classes.splice(i, 1);
+                  break;
+              }
+          }
+          // The className is not found
+          if (length === classes.length) {
+              classes.push(className);
+          }
+          element.className = classes.join(' ');
+      }
+      function toggleAll() {
+          var active = 'active';
+          var elements = getElements();
+          toggleClass(elements.menu_panel,   active);
+          toggleClass(elements.menu_burger,  active);
+      }
+      function handleEvent(e) {
+          var elements = getElements();
+          if (e.target.id === elements.menu_burger.id) {
+              toggleAll();
+              e.preventDefault();
+          } else if (elements.menu.className.indexOf('active') !== -1) {
+              toggleAll();
+          }
+      }
+      document.addEventListener('click', handleEvent);
+  }(this, this.document));
