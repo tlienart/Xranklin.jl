c9e691b1af102c43f6b67d2fabe12452318b4545
tlienart <tlienart@me.com>
2022-12-23 17:26:38 +0100

    xbranch

--- test/_plots/utils.jl
+++ test/_plots/utils.jl
@@ -1,7 +1,7 @@
 function hfun_plot()
     lib = lowercase(get(ENV, "PLIB", ""))
     if lib in ("gaston",)
-        html(read("_$lib.md", String))
+        html(read(joinpath(path(:folder), "_$lib.md"), String))
     else
         html("Lib '$lib' not resolved yet.")
     end
